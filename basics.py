1c643c3110ce1d11198c5462cc773a7301636027
Muhammad Zuhaib <mzuhaibmalik786@gmail.com>
2025-08-08 21:22:19 +0500

    added some code

--- basics.py
+++ basics.py
@@ -1,1 +1,27 @@
+# this is the print statement to print something on screen
 print("hellow world")
+
+# this is how we use variable in python
+# python is dynamically typed language so we do not define the type of variable  
+
+# this is varibale of integer
+integer = 3
+# this is variable of string 
+string = "string"
+# this is variable of float
+float = 39.4
+
+print(float)
+
+# and so on we can make any varibale without writting the type of variable 
+
+# list 
+list = [3,5,"name"]
+
+# dictionaries
+
+dict = {
+    "nam": "zuhaib",
+    "age": 20, 
+}
+print(dict)
